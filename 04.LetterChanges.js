6748337ebf61b7d956c5757e151d92f9f6d6d9ed
mattphoto <github@mattkim.com>
2014-03-22 22:10:17 -0700

    7

--- 04.LetterChanges.js
+++ 04.LetterChanges.js
@@ -1,8 +1,31 @@
 function LetterChanges(str) {
+
   function isLetter(l) {
     return (l <= "z" && l >= "a");
   }
-  function nextLetter {
 
+  function nextLetter(n) {
+    if (isLetter(n)){
+      n = String.fromCharCode(n.charCodeAt(0) + 1);
+    }
+    return n;
   }
+
+  for (var i = 0; i < str.length; i++){
+    nextLetter(str[i]);
+    if (  str[i] === "a" ||
+          str[i] === "e" ||
+          str[i] === "i" ||
+          str[i] === "o" ||
+          str[i] === "u" ) {
+      str[i] = str[i].toUpperCase; // same problem where string methods can be applied.
+    }
+  }
+  return str;
+
 }
+
+
+
+// match against an array
+// (a, e, i, o, u)
