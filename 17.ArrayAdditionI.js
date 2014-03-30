79f03bf772ab9618a541513da233ac467517061c
mattphoto <github@mattkim.com>
2014-03-29 23:01:29 -0700

    c

--- 17.ArrayAdditionI.js
+++ 17.ArrayAdditionI.js
@@ -13,3 +13,75 @@ function ArrayAdditionI(arr) {
   }
   return ;     
 }
+
+
+
+ function ArrayAdditionI(arr) { 
+  arr.sort(function(a,b){return a - b})
+  var largest = arr.pop();
+  var sum = 0;
+
+  for (var i = 0; i &lt; arr.length; i++){
+    sum += arr[i];
+
+    for (var j = 0; j &lt; arr.length; j++){
+      if (i != j) {
+        sum += arr[j];
+        if (sum == largest) {
+          return true;
+        }
+      }
+    }
+
+    for (var k = 0; k &lt; arr.length; k++) {
+      if (i != k) {
+        sum -= arr[k];
+        if (sum == largest) {
+          return true;
+        }
+      }
+    }
+
+    sum = 0;
+  }
+
+  return false;      
+}
+
+
+// not sure why Brian's solution works on test cases
+
+function ArrayAdditionI(arr) { 
+
+  // find largest number
+  var largestNumber = 0;
+  for (var i = 0; i < arr.length; i++) {
+    var number = arr[i];
+    if(number > largestNumber) {
+      largestNumber = number;
+    }
+  }
+  
+  // function to sum array values
+  var sum = function(array) {
+      var total = 0;
+  for (var i = 0; i < array.length; i++) {
+      total += array[i];
+  }
+  return total;
+  };
+  
+  // test combinations
+  for (var i = 0; i < arr.length; i++) {
+    if(sum(arr.slice(i)) == largestNumber) {
+    return true;
+  }
+    console.log(largestNumber, sum(arr.slice(i)), arr.slice(i));
+  }
+    
+  return false;
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+ArrayAdditionI([3, 5, -1, 8, 12]);    
--- 18.LetterCountI.js
+++ 18.LetterCountI.js
@@ -1,10 +1,100 @@
+                          
+
+
+
+
+
+str = "Drivers who have experienced this test have seen a tremendous rise";
+
+console.log(strArr);
+
+
+//
+
+
 function LetterCountI(str) { 
 
-  // code goes here  
+  var splits;
+  var sorts = [];
+  var strArr = [];
+    strArr = str.toLowerCase().split(" "); // array split into individual words
+      for (var i = 0; i < strArr.length; i++) {
+          sorts = (strArr[i].split("").sort()); //each word split and sorted
+          for (var i = 0; i < sorts.length; i++) {
+            console.log(letterCounter(sorts);
+          }
+
+          
+      }
   return str; 
-         
+  
 }
-   
-// keep this function call here 
-// to see how to enter arguments in JavaScript scroll down
-LetterCountI(readline());                            
+
+LetterCountI(str);
+
+//
+
+
+function letterCounter(str){
+  var countLetter = 1;
+  var maxcountLetter = 0;
+  for (var i = 0; i < str.length-1; i++) {
+    if (str[i] === str [i+1]) {
+      countLetter++;
+    }
+    maxcount = countLetter;
+  }
+  if (countLetter === 1) {
+    return -1;
+  }
+  return maxcount;
+}
+
+
+
+
+
+// AM's code
+
+function LetterCountI(str) { 
+  var arr = str.split(' ');
+  
+  var winner = 0;
+  var currentHighest = 0;
+  var stats = {};
+  var foundRepetition = false;
+  
+  // Go through each word in the sentence
+  for (var i=0; i < arr.length; i += 1) {
+     
+     // Go through each character in a word
+     for (var j=0; j < arr[i].length; j += 1 ) {
+       // Make each occurance of a particular letter a key in object; make value of that key hold the count of letters occurance
+       if ( stats[arr[i][j]] === undefined )  {
+           stats[arr[i][j]] = 1;
+       } else {
+           stats[arr[i][j]] += 1;
+       }
+     }
+    
+     // Each key in stats is a letter found in the word    
+     for ( var key in stats ) {
+       if ( stats[key] > 1 ) { foundRepetition = true; }
+       if ( stats[key] > currentHighest) {
+         currentHighest = stats[key];
+         winner = i; 
+       }
+     }
+
+     stats = {};     // reset for new word
+  }
+
+  return foundRepetition ? arr[winner] : -1 ; 
+         
+}
+
+console.log( LetterCountI("Today, is the greatest day ever!") );
+
+console.log( LetterCountI("Hello apple pie") );
+
+console.log( LetterCountI("No words") );
