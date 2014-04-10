439ea88ba254817f2a2a0eb26eca811c5318a869
mattphoto <github@mattkim.com>
2014-04-10 14:49:09 -0700

    a

--- 18.LetterCountI.js
+++ 18.LetterCountI.js
@@ -64,6 +64,39 @@ function LetterCountI(str) {
 
 
 
+// Lindsey's triple nest
+
+function LetterCountI(str) { 
+  var words = str.split(" ");
+  var count = 0;
+  var word = "";
+  // code goes here
+  for (var i = 0; i < words.length; i++) {
+    var wordx = words[i];
+    var sum = 0;
+    for (var j = 0; j < words[i].length; j++) {  
+      var letter = wordx[j];
+      for (var k = 0; k < wordx.length; k++) {
+        if ((j != k) && (letter === wordx[k])){
+          sum += 1;
+        }
+      }
+    }
+    if (sum > count) {
+        count = sum;
+        word = wordx;
+    }
+  }
+  if (count > 0) {
+    return word;
+  } else{
+    return "-1";
+  }
+}
+
+
+
+
 
 // superdewd
 
